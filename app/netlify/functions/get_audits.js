733f16ea17db4433b84e42d704d9ed0578db14ca
codygriffith <cody@alenthea.com>
2022-07-12 01:45:46 -0400

    Update get_audits.js

--- app/netlify/functions/get_audits.js
+++ app/netlify/functions/get_audits.js
@@ -1,14 +1,15 @@
 console.log('blue')
-import fetch from "node-fetch";
+// import fetch from "node-fetch";
 console.log('green')
 
 exports.handler = async function (event, context) {
     // const { identity, user } = context.clientContext;
     // if ( user || identity ) {
         console.log('red')
-        const rawReports = await fetch(process.env.API_ENDPOINT);
-		const reports = await rawReports.json();
-		console.log(reports);
+        let reports = {};
+        // const rawReports = await fetch(process.env.API_ENDPOINT);
+		// const reports = await rawReports.json();
+		// console.log(reports);
         return {
             statusCode: 200,
             body: JSON.stringify({ reports }),
