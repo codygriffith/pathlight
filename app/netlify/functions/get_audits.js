7dd1547e8a590b1243cd5e4b50cfa7f73a758dd3
codygriffith <cody@alenthea.com>
2022-07-12 01:31:17 -0400

    Update get_audits.js

--- app/netlify/functions/get_audits.js
+++ app/netlify/functions/get_audits.js
@@ -4,9 +4,7 @@ export async function handler (event, context) {
     // const { identity, user } = context.clientContext;
     // if ( user || identity ) {
         console.log('red')
-        const rawReports = await fetch(process.env.API_ENDPOINT, {
-			method: 'GET'
-		});
+        const rawReports = await fetch(process.env.API_ENDPOINT);
 		const reports = await rawReports.json();
 		console.log(reports);
         return {
