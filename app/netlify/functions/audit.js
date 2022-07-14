1ca0a0869831240b9a73d830043d82749b7eaf41
codygriffith <cody@alenthea.com>
2022-07-14 02:39:37 -0400

    Update audit.js

--- app/netlify/functions/audit.js
+++ app/netlify/functions/audit.js
@@ -7,6 +7,13 @@ exports.handler = async function (event, context) {
     console.log('red');
     console.log(event.body)
     const url = JSON.parse(event.body);
+
+    let body = {url, id}
+
+    console.log(body)
+
+    let res = await fetch(`https://sqs.us-east-1.amazonaws.com/054477282436/pathlight-MySqsQueue-7Av0SPeLhEXe/?Action=SendMessage&MessageBody=${body}`);
+
     return {
         statusCode: 200,
         body: JSON.stringify({ url: url, id: id }),
