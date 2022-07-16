659d1b591fd51ef4b3d11d871047bf2df4f1f7eb
codygriffith <cody@alenthea.com>
2022-07-16 18:26:41 -0400

    Update index.svelte

--- app/src/routes/index.svelte
+++ app/src/routes/index.svelte
@@ -71,7 +71,7 @@
 	<form class="border-b-[.1vw] w-[30vw]">
 		
 		<label for="url">https://</label>
-		<input name="url" bind:value={url} class="text-center w-[75.5%]" placeholder="pathlight.com"/>
+		<input name="url" bind:value={url} class="text-center w-[75.5%]" placeholder="pathlight.dev"/>
 		<button on:click={requestAudit} type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Scan Url</button>
 	</form>
 
