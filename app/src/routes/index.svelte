03b2d26de7cad5c2c57bee1aaed4b9f05d8b09ea
codygriffith <cody@alenthea.com>
2022-07-17 08:26:18 -0400

    Revert "add audit progress. Cleanup."
    This reverts commit 810fb7e7165f12657b83861d5225e922439b0fda.

--- app/src/routes/index.svelte
+++ app/src/routes/index.svelte
@@ -3,77 +3,59 @@
 </script>
 
 <script>
-	// clearInterval(interval);
 	import { GET, POST } from '$lib/store.js';
 	import { onMount } from 'svelte';
-	import FakeProgress from 'fake-progress';
-
-	let response = { reports: { items: [{}] } };
-	let pendingAudits = [];
-	let lastEvaluatedKey;
-	let url = '';
+	let response = { reports: {items:[{}]} };
 	let reports;
+	let url = '';
+	let lastEvaluatedKey;
+	// import process from 'process';
+	// import dotenv from 'dotenv';
+	// dotenv.config({ path: '.env' });
 
-	var onEachSecond = function () {
-		pendingAudits = pendingAudits;
-	};
+	// const API_ENDPOINT = process.env.API_ENDPOINT;
 
-	var interval = setInterval(onEachSecond, 500);
-
-	var onEnd = function () {
-		pendingAudits = pendingAudits;
-	};
-
-	var checkProgress = async function () {
-		console.log('self');
-		response = await GET('/get_audits');
-		reports = response.reports.items;
-		reports.forEach((report) => {
-			report.timestamp = new Date(report.timestamp);
-		});
-		reports = reports.sort((a, b) => b.timestamp - a.timestamp).reverse();
-		lastEvaluatedKey = response.reports.lastEvaluatedKey || null;
-		pendingAudits.forEach((audit) => {
-			let found = reports.find((el) => el.id == audit.id);
-			if (found) {
-				audit.progress.end();
-			}
-			pendingAudits = pendingAudits;
-		});
-	};
+	// console.log(API_ENDPOINT);
 
 	async function requestAudit() {
-		let res = await POST('/audit', url);
-		console.log(res);
-		res.progress = new FakeProgress({
-			timeConstant: 50000,
-			autoStart: true
-		});
-		pendingAudits.push(res);
-		pendingAudits = pendingAudits;
+		const times = 100;
+		for(var i = 0; i < times; i++){
+    		// doSomething();
+			let res  = await POST('/audit', url);
+			console.log(res)
+		}
 		url = '';
+		// let res = await fetch(`https://sqs.us-east-1.amazonaws.com/054477282436/pathlight-MySqsQueue-7Av0SPeLhEXe/?Action=SendMessage&MessageBody=${url}`);
 	}
 
 	async function loadReports() {
-		let res = await GET(`/get_audits?exclusiveStartKey=${lastEvaluatedKey.id}`);
-		lastEvaluatedKey = res.reports.lastEvaluatedKey || null;
-		reports = reports.push(res.reports.items);
-		reports = reports;
+		let res  = await GET(`/get_audits?exclusiveStartKey=${lastEvaluatedKey.id}`);
+		// lastEvaluatedKey = res.reports.lastEvaluatedKey || null;
+		console.log(res.reports.items)
+		console.log(res.reports.lastEvaluatedKey)
+		lastEvaluatedKey = res.reports.lastEvaluatedKey || null
+		reports = reports.concat(res.reports.items);
+		// lastEvaluatedKey = res.reports.
+		// reports = response.reports.items;
+		// console.log(reports);
 	}
+
+	console.log('red');
 
 	onMount(async () => {
 		// if (!isLoggedIn) {
 		// 	goto('/');
 		// }
+		console.log('red');
 		response = await GET('/get_audits');
+		// console.log(response.reports)
 		reports = response.reports.items;
 		lastEvaluatedKey = response.reports.lastEvaluatedKey || null;
-		reports.forEach((report) => {
-			report.timestamp = new Date(report.timestamp);
-		});
-		reports = reports.sort((a, b) => b.timestamp - a.timestamp).reverse();
-		setInterval(checkProgress, 5000);
+		console.log(reports);
+		console.log(lastEvaluatedKey)
+		// console.log(JSON.stringify(reports[13].rawJson))
 	});
+	// loadReports()
 </script>
 
 <svelte:head>
@@ -83,67 +65,28 @@
 
 <!-- <div class="relative overflow-hidden">Hello!</div> -->
 <div class="flex flex-col items-center m-auto w-[100%] mt-16 mb-4">
-	<!-- <button on:click={() => p.start()} type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">End</button> -->
-
 	<h1 class="text-4xl font-bold mb-8">Website Audit</h1>
 
+
 	<form class="border-b-[.1vw] w-[30vw]">
+		
 		<label for="url">https://</label>
-		<input name="url" bind:value={url} class="text-center w-[75.5%]" placeholder="pathlight.dev" />
+		<input name="url" bind:value={url} class="text-center w-[75.5%]" placeholder="pathlight.dev"/>
 		<button on:click={requestAudit} type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Scan Url</button>
 	</form>
 
-	<!-- {#if pendingAudits.length > 0} -->
-	{#if reports !== undefined && reports !== null && pendingAudits.length > 0}
-		<div class="mt-8 flex flex-col mb-10">
-			<div class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
-				<div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
-					<div class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
-						<table class="min-w-full divide-y divide-gray-300">
-							<thead class="bg-gray-50 justify-between text-sm">
-								<tr>
-									<th scope="col" class="font-normal pl-4 pr-3 text-gray-900 sm:pl-6 text-center">Url</th>
-									<th scope="col" class="font-normal px-3 text-gray-900 text-center">Progress</th>
-									<th scope="col" class="relative pl-3 pr-4 sm:pr-6" />
-								</tr>
-							</thead>
-							<tbody class="divide-y divide-gray-200 bg-white flex-col-reverse text-sm">
-								{#each pendingAudits as report}
-									<tr>
-										<td class="whitespace-nowrap py-2 pl-4 pr-3 font-normal text-gray-900 sm:pl-6">{report.url}</td>
-										<td class="whitespace-nowrap py-2 pl-4 pr-3 font-normal text-gray-900 sm:pl-6 w-[20vw]">
-											<div class="w-full bg-gray-200 rounded-full dark:bg-gray-700/20">
-												<div class="bg-blue-500 text-[.25vw] font-normal text-blue-100 text-center leading-none rounded-full transition-all duration-[4000ms]" style="width: {(report.progress.progress * 100).toFixed(3)}%">&nbsp;</div>
-											</div>
-										</td>
-
-										<td class="relative whitespace-nowrap py-2 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
-											{#if report.progress.progress * 100 >= 100}
-												<a href="/report/view?id={report.id}" class="text-indigo-600 font-normal hover:text-indigo-900">View Report</a>
-											{:else}
-												<a href="/report/view?id={report.id}" style="pointer-events: none" class="text-indigo-600 font-normal hover:text-indigo-900">View Report</a>
-											{/if}
-										</td>
-
-									</tr>
-								{/each}
-							</tbody>
-						</table>
-					</div>
-				</div>
-			</div>
-		</div>
-	{/if}
-	
-
-	<div class="text-center mb-3">
-		<h1 class="text-3xl font-medium mt-10">Latest Scans</h1>
-		{#if reports !== undefined && reports !== null}
-			<p>{reports.length} total reports</p>
-		{/if}
-	</div>
+	<h1 class="text-3xl font-medium mb-3 mt-10">Latest Scans</h1>
 
 	<div class="px-4 sm:px-6 lg:px-8 w-[70vw]">
+		<!-- <div class="sm:flex sm:items-center">
+			<div class="sm:flex-auto">
+				<h1 class="text-xl font-semibold text-gray-900">Users</h1>
+				<p class="mt-2 text-sm text-gray-700">A list of all the users in your account including their name, title, email and role.</p>
+			</div>
+			<div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
+				<button type="button" class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto">Add user</button>
+			</div>
+		</div> -->
 		<div class="mt-8 flex flex-col mb-10">
 			<div class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
 				<div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
@@ -157,10 +100,8 @@
 									<th scope="col" class="px-3 py-3.5 text-left font-semibold text-gray-900">Accessibility</th>
 									<th scope="col" class="px-3 py-3.5 text-left font-semibold text-gray-900">Best Practices</th>
 									<th scope="col" class="px-3 py-3.5 text-left font-semibold text-gray-900">SEO</th>
-									<th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-6 text-sm">
-										{#if reports !== undefined && reports !== null}
-											{reports.length}
-										{/if}
+									<th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-6">
+										<span class="sr-only">Edit</span>
 									</th>
 								</tr>
 							</thead>
@@ -169,7 +110,7 @@
 									{#each reports.reverse() as report}
 										<tr>
 											<td class="whitespace-nowrap py-4 pl-4 pr-3 font-medium text-gray-900 sm:pl-6">{report.url}</td>
-											<td class="whitespace-nowrap px-3 py-4 text-gray-500">{report.timestamp.toUTCString()}</td>
+											<td class="whitespace-nowrap px-3 py-4 text-gray-500">{report.timestamp}</td>
 											<td class="whitespace-nowrap px-3 py-4 text-gray-500">{report.scores.performance}</td>
 											<td class="whitespace-nowrap px-3 py-4 text-gray-500">{report.scores.accessibility}</td>
 											<td class="whitespace-nowrap px-3 py-4 text-gray-500">{report.scores.bestPractices}</td>
@@ -183,6 +124,7 @@
 									Loading
 								{/if}
 
+								<!-- More people... -->
 							</tbody>
 						</table>
 					</div>
@@ -190,7 +132,23 @@
 			</div>
 		</div>
 		<button on:click={loadReports} type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Scan Url</button>
+		
 	</div>
+
+	<!-- {#if response.reports[0].url !== undefined}
+		{#each response.reports as report}
+			<div class="flex w-[70%] text-lg justify-evenly">
+				<p class="w-[25%] text-center">{report.url}</p>
+				<p class="w-[35%] text-center">{report.timestamp}</p>
+				<p class="w-[5%] text-center">{report.scores.performance}</p>
+				<p class="w-[5%] text-center">{report.scores.accessibility}</p>
+				<p class="w-[5%] text-center">{report.scores.bestPractices}</p>
+				<p class="w-[5%] text-center">{report.scores.seo}</p>
+			</div>
+		{/each}
+	{:else}
+		Loading
+	{/if} -->
 </div>
 
 <style>
--- src/handlers/audit-url.js
+++ src/handlers/audit-url.js
@@ -1,7 +1,14 @@
+// const axios = require('axios')
+// const url = 'http://checkip.amazonaws.com/';
 let response;
 const lighthouse = require('lighthouse');
+const { v4: uuidv4 } = require('uuid');
+// const chromeLauncher = require('chrome-launcher');
 const chromium = require('chrome-aws-lambda');
 const { URL } = require('url');
+
+
+// Create a DocumentClient that represents the query to add an item
 const dynamodb = require('aws-sdk/clients/dynamodb');
 const docClient = new dynamodb.DocumentClient();
 
@@ -9,8 +16,10 @@ const docClient = new dynamodb.DocumentClient();
 const reportTable = process.env.REPORT_TABLE;
 const rawReportTable = process.env.RAW_REPORT_TABLE;
 
+
 exports.auditUrlHandler = async (event, context) => {
     try {
+        // const id = uuidv4();
         const req = JSON.parse(event.Records[0].body);
         const id = req.id;
         const url = req.url;
@@ -27,10 +36,18 @@ exports.auditUrlHandler = async (event, context) => {
 
         await page.goto(`http://${url}` || 'https://alenthea.com');
 
+        // const chrome = await chromeLauncher.launch({ chromeFlags: ['--headless','--no-sandbox','--disable-gpu','--disable-dev-shm-usage'] });
         const options = { logLevel: 'info', onlyCategories: ['performance','accessibility','best-practices','seo'], skipAudits: ['full-page-screenshot','screenshot-thumbnails','script-treemap-data','critical-request-chains','final-screenshot','user-timings','network-requests'], output: 'html', port: (new URL(chrome2.wsEndpoint())).port };
         const runnerResult = await lighthouse(`http://${url}`, options);
 
         console.log('Report is done for', runnerResult.lhr.finalUrl);
+
+        // console.log(runnerResult)
+
+        // console.log(JSON.stringify(runnerResult))
+
+        // console.log(runnerResult.lhr)
+        // console.log(JSON.stringify(runnerResult.lhr))
 
         const performanceScore = runnerResult.lhr.categories["performance"].score;
         const accessibilityScore = runnerResult.lhr.categories["accessibility"].score;
@@ -38,6 +55,18 @@ exports.auditUrlHandler = async (event, context) => {
         const seoScore = runnerResult.lhr.categories["seo"].score;
         // const pwaScore = runnerResult.lhr.categories["pwa"].score;
 
+        console.log(JSON.stringify(runnerResult.lhr))
+        // runnerResult.lhr.audits['full-page-screenshot'].details.screenshot.data = "data:image/jpeg;"
+        // runnerResult.lhr.audits['full-page-screenshot'].details.nodes = "{}"
+        // console.log(runnerResult.lhr.audits['screenshot-thumbnails'].details.items)
+        // runnerResult.lhr.audits['screenshot-thumbnails'].details.items.splice(4, 5);
+        // console.log(runnerResult.lhr.audits['screenshot-thumbnails'].details.items)
+        // console.log(runnerResult.lhr.audits['full-page-screenshot'])
+
+
+        // delete runnerResult.lhr.audits["full-page-screenshot"]
+        // console.log(runnerResult.lhr.audits)
+        
         var params = {
             TableName: reportTable,
             Item: {
@@ -51,10 +80,13 @@ exports.auditUrlHandler = async (event, context) => {
                     seo: seoScore
                     // pwa: pwaScore
                 },
+                // rawJson: JSON.stringify(runnerResult.lhr)
             }
         };
 
         let result = await docClient.put(params).promise();
+
+        console.log(result)
 
         var params = {
             TableName: rawReportTable,
@@ -65,6 +97,9 @@ exports.auditUrlHandler = async (event, context) => {
         };
 
         result = await docClient.put(params).promise();
+
+        console.log(result)
+
 
         response = {
             'statusCode': 201,
