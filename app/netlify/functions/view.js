1813622bfdedc5e878d45d21c8bc52abc292fd6f
codygriffith <cody@alenthea.com>
2022-07-17 19:34:17 -0400

    Update view.js

--- app/netlify/functions/view.js
+++ app/netlify/functions/view.js
@@ -1,6 +1,6 @@
 console.log('heheh')
-const fetch = require( "node-fetch");
-const reportHtml = require( "./report.txt");
+import fetch from "node-fetch";
+// import reportHtml from "./report.txt";
 console.log('ttttttttt')
 
 exports.handler = async function (event, context) {
@@ -12,7 +12,7 @@ exports.handler = async function (event, context) {
 
   return {
     statusCode: 200,
-    body: reportHtml.replace('__GET_LIGHTHOUSE_JSON__', report.rawJson),
+    body: report.rawJson,
   };
   // }
 };
