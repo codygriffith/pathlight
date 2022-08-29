65ba5cad5ac5ab751531aae15f3b3dc9c87d7b36
codygriffith <cody@alenthea.com>
2022-08-29 19:03:31 -0400

    Update index.svelte

--- app/src/routes/index.svelte
+++ app/src/routes/index.svelte
@@ -87,10 +87,10 @@
 
 	<h1 class="text-4xl font-bold mb-8">Website Audit</h1>
 
-	<form class="border-b-[.1vw] w-[35vw] flex justify-between h-[2.5vw]">
+	<form class="border-b-[.1vw] lg:w-[35vw] flex justify-between">
 		<label for="url" class="m-2 inline-flex items-center px-3 py-2 border border-transparent text-base">https://</label>
 		<input name="url" bind:value={url} class="text-center w-[70%]" placeholder="pathlight.dev" />
-		<button on:click={requestAudit} type="button" class="m-2 inline-flex items-center px-3 py-1 border border-transparent text-sm leading-4 font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Scan Url</button>
+		<button on:click={requestAudit} type="button" class="whitespace-nowrap m-2 inline-flex items-center px-3 py-1 border border-transparent text-sm leading-4 font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Scan Url</button>
 	</form>
 
 	<!-- {#if pendingAudits.length > 0} -->
