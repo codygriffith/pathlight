90566d19fb1fd7a8d6b25ee42873aad1d9fef1bd
codygriffith <cody@alenthea.com>
2022-08-29 19:35:17 -0400

    Update mobile layout of homepage

--- app/src/routes/index.svelte
+++ app/src/routes/index.svelte
@@ -87,7 +87,7 @@
 
 	<h1 class="text-4xl font-bold mb-8">Website Audit</h1>
 
-	<form class="border-b-[.1vw] lg:w-[35vw] flex justify-between">
+	<form class="border-b-[.1vw] md:w-[65vw] lg:w-[35vw] flex justify-between">
 		<label for="url" class="m-2 inline-flex items-center px-3 py-2 border border-transparent text-base">https://</label>
 		<input name="url" bind:value={url} class="text-center w-[70%]" placeholder="pathlight.dev" />
 		<button on:click={requestAudit} type="button" class="whitespace-nowrap m-2 inline-flex items-center px-3 py-1 border border-transparent text-sm leading-4 font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Scan Url</button>
@@ -143,20 +143,38 @@
 		{/if}
 	</div>
 
-	<div class="px-4 sm:px-6 lg:px-8 w-[70vw]">
+	<div class="px-4 sm:px-6 lg:px-8 min-w-full lg:min-w-0 lg:w-[70vw]">
 		<div class="mt-8 flex flex-col mb-10">
 			<div class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
-				<div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
-					<div class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
+				<div class="inline-block min-w-full py-2 align-middle lg:px-8">
+					<div class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 lg:rounded-lg">
 						<table class="min-w-full divide-y divide-gray-300">
-							<thead class="bg-gray-50 justify-between text-lg">
+							<thead class="bg-gray-50 justify-between text-sm lg:text-lg">
 								<tr>
 									<th scope="col" class="py-4 pl-4 pr-3 text-left w-[20%] font-semibold text-gray-900 sm:pl-6">Url</th>
 									<th scope="col" class="px-3 py-3.5 text-left font-semibold text-gray-900">Timestamp</th>
-									<th scope="col" class="px-3 py-3.5 text-left font-semibold text-gray-900">Performance</th>
-									<th scope="col" class="px-3 py-3.5 text-left font-semibold text-gray-900">Accessibility</th>
-									<th scope="col" class="px-3 py-3.5 text-left font-semibold text-gray-900">Best Practices</th>
-									<th scope="col" class="px-3 py-3.5 text-left font-semibold text-gray-900">SEO</th>
+									<th scope="col" class="px-3 py-3.5 text-left font-semibold text-gray-900 whitespace-nowrap">
+										<div class="flex">
+											<div>Perf</div>
+											<div class="lg:hidden">.</div>
+											<div class="hidden lg:inline">ormance</div>
+										</div>
+									</th>
+									<th scope="col" class="px-3 py-3.5 text-left font-semibold text-gray-900 whitespace-nowrap">
+										<div class="flex">
+											Access
+											<span class="lg:hidden">.</span>
+											<span class="hidden lg:inline">ibility</span>
+										</div>
+									</th>
+									<th scope="col" class="px-3 py-3.5 text-left font-semibold text-gray-900 whitespace-nowrap">
+										<div class="flex">
+											Best Prac
+											<span class="lg:hidden">.</span>
+											<span class="hidden lg:block">tices</span>
+										</div>
+									</th>
+									<th scope="col" class="px-3 py-3.5 text-left font-semibold text-gray-900 whitespace-nowrap">SEO</th>
 									<th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-6 text-sm">
 										<!-- {#if reports !== undefined && reports !== null} -->
 										<!-- {reports.length} -->
