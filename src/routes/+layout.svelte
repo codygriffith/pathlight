b48eae8ba7a983daf88b6a5a2ded693703a72ae2
codygriffith <35708679+codygriffith@users.noreply.github.com>
2023-02-11 02:29:04 -0500

    add nav bar, dark/light mode

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,11 @@
 <script>
 	import '../app.css';
+	import Header from './Header.svelte';
 </script>
 
-<div class="flex flex-col h-full justify-center items-center dark:text-white">
-	<slot />
+<div class="flex flex-col min-h-full items-center dark:text-white dark:bg-gray-700">
+	<Header />
+	<div class="flex flex-col h-full items-center m-auto">
+		<slot />
+	</div>
 </div>
