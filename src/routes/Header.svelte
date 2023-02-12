e38b5af0d7c8116db2208da367aeec37c2ea1003
codygriffith <35708679+codygriffith@users.noreply.github.com>
2023-02-12 06:01:31 -0500

    Update Header.svelte

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -3,16 +3,16 @@
 </script>
 
 <div class="static top-0 bg-gray-200 dark:bg-slate-800 w-full">
-	<div class="flex justify-between items-center px-4 py-6 sm:px-6 md:justify-start md:space-x-10">
+	<div class="flex justify-between items-center w-full px-4 py-6 sm:px-6">
 		<a href="#" class="flex">
 			<img class="h-8 w-auto sm:h-10 hidden dark:flex" src="/Pathlight_logo-light.svg" alt="" />
 			<img class="h-8 w-auto sm:h-10 dark:hidden" src="/Pathlight_logo-dark.svg" alt="" />
 		</a>
-		<div class="flex w-full justify-end">
+		<!-- <div class="flex w-full justify-end">
 			<nav class="flex space-x-10">
 				<a href="#" class="text-base font-medium text-gray-500 dark:text-gray-200 hover:text-gray-900 text-end"> About </a>
 			</nav>
-		</div>
+		</div> -->
 		<ThemeToggle />
 	</div>
 </div>
