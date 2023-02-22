89f92863a42f4c3434ba619bd7a2eacd474cba64
codygriffith <35708679+codygriffith@users.noreply.github.com>
2023-02-22 03:46:02 -0500

    logo image alt text

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -5,8 +5,8 @@
 <div class="static top-0 bg-gray-200 dark:bg-slate-800 w-full">
 	<div class="flex justify-between items-center w-full max-w-[90rem] m-auto p-5 sm:px-16">
 		<a href="/" class="flex">
-			<img class="h-8 w-auto sm:h-10 hidden dark:flex" src="/Pathlight_logo-light.svg" alt="" />
-			<img class="h-8 w-auto sm:h-10 dark:hidden" src="/Pathlight_logo-dark.svg" alt="" />
+			<img class="h-8 w-auto sm:h-10 hidden dark:flex" src="/Pathlight_logo-light.svg" alt="Pathlight logo" />
+			<img class="h-8 w-auto sm:h-10 dark:hidden" src="/Pathlight_logo-dark.svg" alt="Pathlight logo" />
 		</a>
 		<div class="flex w-full justify-end">
 			<nav class="flex px-8">
