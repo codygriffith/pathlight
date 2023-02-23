1cb5d19951b1e0dc6ed35688163c515bcc1bd0da
codygriffith <35708679+codygriffith@users.noreply.github.com>
2023-02-22 21:29:59 -0500

    create privacy policy

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -1,3 +1,8 @@
-<footer class="flex w-full items-center justify-center py-2 bg-gray-200 dark:bg-slate-800 text-slate-700 dark:text-slate-300">
+<footer class="flex w-full items-center justify-between py-2 px-16 bg-gray-200 dark:bg-slate-800 text-slate-700 dark:text-slate-300">
 	<span class="text-sm">© 2023 &#8226; All Rights Reserved</span>
+	<ul class="flex flex-wrap items-center mt-3 text-sm sm:mt-0">
+		<li>
+			<a href="/privacy-policy" class="mr-4 hover:underline md:mr-6">Privacy Policy</a>
+		</li>
+	</ul>
 </footer>
