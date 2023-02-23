543bbfa54ca9684045c498f466bca4e43ff41a7f
codygriffith <35708679+codygriffith@users.noreply.github.com>
2023-02-22 19:34:30 -0500

    add footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,17 @@
 <script>
+	// @ts-nocheck
+
 	import '../app.css';
 	import Header from './Header.svelte';
+	import Footer from './Footer.svelte';
 </script>
 
-<div class="flex flex-col w-full min-h-full items-center dark:text-white dark:bg-gray-700">
-	<Header />
-	<div class="flex flex-col w-full items-center">
-		<slot />
+<div class="flex flex-col w-full min-h-full justify-between items-center dark:text-white dark:bg-gray-700">
+	<div class="w-full">
+		<Header />
+		<div class="flex flex-col w-full items-center">
+			<slot />
+		</div>
 	</div>
+	<Footer />
 </div>
---
+++ src/routes/Footer.svelte
@@ -0,0 +1,3 @@
+<footer class="flex w-full items-center justify-center py-2 bg-gray-200 dark:bg-slate-800 text-slate-700 dark:text-slate-300">
+	<span class="text-sm">© 2023 &#8226; All Rights Reserved</span>
+</footer>
