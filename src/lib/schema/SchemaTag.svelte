f0dd715f1c2c28d7b02f139054e20a654e54eb81
codygriffith <35708679+codygriffith@users.noreply.github.com>
2023-02-25 03:14:15 -0500

    Create SchemaTag.svelte

---
+++ src/lib/schema/SchemaTag.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { serializeSchema } from '$lib/schema';
+	import type { Schema } from '$lib/schema';
+
+	export let schema: Schema;
+</script>
+
+<svelte:head>
+	{@html serializeSchema(schema)}
+</svelte:head>
