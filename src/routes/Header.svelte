eafa2ac9c46f03dfede0054670bb8746f1aed2a1
codygriffith <35708679+codygriffith@users.noreply.github.com>
2023-02-25 05:59:04 -0500

    Update Header.svelte

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -2,17 +2,15 @@
 	import ThemeToggle from './ThemeToggle.svelte';
 </script>
 
-<div class="static top-0 bg-gray-200 dark:bg-slate-800 w-full">
-	<div class="flex justify-between items-center w-full max-w-[90rem] m-auto p-3 sm:p-5 sm:px-16">
-		<a href="/" class="flex">
-			<img width="151px" height="40px" class="h-12 md:h-10 w-auto hidden dark:flex" src="/Pathlight_logo-light.svg" alt="Pathlight logo" />
-			<img width="151px" height="40px" class="h-12 md:h-10 w-auto dark:hidden" src="/Pathlight_logo-dark.svg" alt="Pathlight logo" />
-		</a>
-		<div class="flex w-full justify-end">
-			<nav class="flex pr-5">
-				<a href="/our-guide" class="text-base font-medium text-gray-600 dark:text-gray-200 hover:text-gray-900 text-end">Our Guide</a>
-			</nav>
-		</div>
-		<ThemeToggle />
+<header class="flex flex-row w-full items-center justify-between p-5 lg:px-20 2xl:px-72 bg-gray-200 dark:bg-slate-800 text-slate-700 dark:text-slate-300">
+	<a href="/" class="flex">
+		<img width="151px" height="40px" class="h-12 md:h-10 w-auto hidden dark:flex" src="/Pathlight_logo-light.svg" alt="Pathlight logo" />
+		<img width="151px" height="40px" class="h-12 md:h-10 w-auto dark:hidden" src="/Pathlight_logo-dark.svg" alt="Pathlight logo" />
+	</a>
+	<div class="flex w-full justify-end">
+		<nav class="flex pr-5">
+			<a href="/our-guide" class="text-base font-medium text-gray-600 dark:text-gray-200 hover:text-gray-900 text-end">Our Guide</a>
+		</nav>
 	</div>
-</div>
+	<ThemeToggle />
+</header>
