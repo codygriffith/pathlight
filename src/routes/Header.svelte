e80c5b7f479f629f86905c00d9a71884e5550a32
codygriffith <35708679+codygriffith@users.noreply.github.com>
2023-02-26 16:57:07 -0500

    Update Header.svelte

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -2,7 +2,7 @@
 	import ThemeToggle from './ThemeToggle.svelte';
 </script>
 
-<header class="flex flex-row w-full items-center justify-between p-5 lg:px-20 2xl:px-72 bg-gray-200 dark:bg-slate-800 text-slate-700 dark:text-slate-300">
+<header class="flex flex-row w-full items-center justify-between p-5 sm:px-12 lg:px-20 2xl:px-72 bg-gray-200 dark:bg-slate-800 text-slate-700 dark:text-slate-300">
 	<a href="/" class="flex">
 		<img width="151px" height="40px" class="h-12 md:h-10 w-auto hidden dark:flex" src="/Pathlight_logo-light.svg" alt="Pathlight logo" />
 		<img width="151px" height="40px" class="h-12 md:h-10 w-auto dark:hidden" src="/Pathlight_logo-dark.svg" alt="Pathlight logo" />
