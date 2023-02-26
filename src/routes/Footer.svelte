f138cbf76ac4aa55babf0266295de945e72d6ed1
codygriffith <35708679+codygriffith@users.noreply.github.com>
2023-02-26 16:57:09 -0500

    Update Footer.svelte

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -1,4 +1,4 @@
-<footer class="flex flex-col-reverse sm:flex-row w-full items-center justify-between p-5 lg:px-20 2xl:px-72 bg-gray-200 dark:bg-slate-800 text-slate-700 dark:text-slate-300">
+<footer class="flex flex-col-reverse sm:flex-row w-full items-center justify-between p-5 sm:px-12 lg:px-20 2xl:px-72 bg-gray-200 dark:bg-slate-800 text-slate-700 dark:text-slate-300">
 	<span class="my-2 sm:m-0">© 2023 &#8226; All Rights Reserved</span>
 	<div class="flex sm:flex-row flex-wrap my-2 sm:m-0 items-center">
 		<a href="/about" class="mr-4 hover:underline md:mr-6 p-1">About</a>
