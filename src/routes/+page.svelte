bacad51fd7cce4d3a7fe0a38acf34496c9225b30
codygriffith <35708679+codygriffith@users.noreply.github.com>
2024-01-02 20:46:23 -0500

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -80,7 +80,7 @@
 	<div role="status">
 		<svg
 			aria-hidden="true"
-			class="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+			class="w-20 h-20 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600 m-4"
 			viewBox="0 0 100 101"
 			fill="none"
 			xmlns="http://www.w3.org/2000/svg"
